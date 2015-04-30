321e68e50e062bae5c70c777110de3b248b8a292
Leif Andersen <leif@leifandersen.net>
2015-04-30 17:40:40 -0400

    Updated info.rkt information.

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection "readline-gpl")
 (define deps '("base"
                "rackunit-lib"))
-(define pkg-desc "Description Here")
-(define version "0.0")
+(define pkg-desc "Readline GPL")
+(define version "1.0")
 (define pkg-authors '(leif))
 (define copy-shared-files '("private/readline-lib.rkt"))
